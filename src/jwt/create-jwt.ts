9bbd7a2c8095f0e10e8e1715c7435957fc434def
James Young <hello@jamesyoung.ch>
2022-10-16 04:35:17 +0200

    feat: add scripts to create jwt

---
+++ src/jwt/create-jwt.ts
@@ -0,0 +1,19 @@
+import { readFileSync } from 'fs';
+import JWT from 'jsonwebtoken';
+
+const pemFilePath = "/Users/jamesyoung/gh-pem/template-browser.2022-10-13.private-key.pem"; // TODO make config
+const ghAppId = "247562"; // TODO make config
+
+export const createJwt = (): string => {
+  const privateKey = readFileSync(pemFilePath, { encoding: 'utf-8' });
+
+  const jwt = JWT.sign({}, privateKey, {
+    algorithm: 'RS256',
+    issuer: ghAppId,
+    expiresIn: '9 minutes',
+  });
+
+  console.debug(`Created JWT: ${jwt}`);
+
+  return jwt;
+};
